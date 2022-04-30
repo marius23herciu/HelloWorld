d01fd1b5255c3e45ac88488822f8d8f691b2b5fe
sebastian popescu <sebastianh123@yahoo.com>
2022-04-30 14:04:59 +0300

    int x = int.Parse(Console.ReadLine());

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!2");
+            Console.WriteLine("introduceti lungimea");
+            int lungime = int.Parse(Console.ReadLine());
+            int latime = 2;
+            int arie = lungime * latime;
+            Console.WriteLine(arie);
         }
 
     }
